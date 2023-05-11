b2495edd29545e12cb6aab6e18d491f931cc95eb
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-11 21:40:36 +0300

    added swithc constructor

---
+++ src/main/java/org/example/SwitchDemo.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class SwitchDemo {
+    public static void main(String args[]) {
+        int i;
+        for (i = 0; i < 10; i++){
+            switch (i) {
+                case 0:
+                    System.out.println(i + " == 0");
+//                    break;
+                case 1:
+                    System.out.println(i + " == 1");
+//                    break;
+                case 2:
+                    System.out.println(i + " == 2");
+//                    break;
+                case 3:
+                    System.out.println(i + " == 3");
+//                    break;
+                case 4:
+                    System.out.println(i + " == 4");
+//                    break;
+//                case 5:
+//                    System.out.println(i + " == or > 5");
+//                    System.out.println(i + " == 0");
+                }
+            System.out.println();
+            }
+        }
+    }
+
