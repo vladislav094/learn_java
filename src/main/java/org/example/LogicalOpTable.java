df147d358b6773372caebbbf078db4f3297985e4
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-11 19:38:54 +0300

    added my first commit with code in this repo

--- src/main/java/org/example/LogicalOpTable.java
+++ src/main/java/org/example/LogicalOpTable.java
@@ -1,5 +1,34 @@
 package org.example;
 
 public class LogicalOpTable {
+    public static void main(String args[]) {
+        boolean p, q;
+        byte b;
+        int i;
+        b = 10;
+        b = (byte) ( b * b);
+        System.out.print("One");
+        System.out.print("Two");
+        System.out.print("Three");
+        System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+        p = true; q = true;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p&q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
 
+        p = true; q = false;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = false; q = true;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = false; q = false;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+    }
 }
