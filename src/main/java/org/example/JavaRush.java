0aa985be6c73c0c78236fe53e5b376b39051212e
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-14 21:10:23 +0300

    start study on JavaRush

---
+++ src/main/java/org/example/JavaRush.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class JavaRush {
+    public static void main(String[] args) {
+        String text = "Ellie is very smart";
+        for (int i = 0; i < 5; i++) {
+            System.out.println(text);
+        }
+
+    }
+}
