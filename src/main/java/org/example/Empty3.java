f9770242c982012ba7d4cb7d690cd08c2bc81fb7
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-14 22:37:14 +0300

    another one loop in java - while

---
+++ src/main/java/org/example/Empty3.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Empty3 {
+    public static void main(String args[]) {
+        int i;
+        int sum = 0;
+        for (i = 1; i <= 5; sum += i++);
+
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/org/example/WhileDemo.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class WhileDemo {
+    public static void main(String args[]) {
+        char ch;
+
+        ch = 'a';
+        while (ch <= 'z') {
+            System.out.println(ch);
+            ch += 1;
+        }
+    }
+}
