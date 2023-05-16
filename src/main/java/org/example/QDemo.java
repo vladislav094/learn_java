14b6e42eeafb7301de1229390be11feb76170eb7
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-16 22:08:18 +0300

    i start delop stack im java arrays

--- src/main/java/org/example/QDemo.java
+++ src/main/java/org/example/QDemo.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class QDemo {
+    char q[];
+    int putloc, getloc;
+    QDemo(int size) {
+        q = new char[size];
+        putloc = getloc = 0;
+    }
+    void put(char ch) {
+        if(putloc == q.length) {
+            System.out.println(" - Очеред заполнена");
+            return;
+        }
+        q[putloc++] = ch;
+    }
 }
