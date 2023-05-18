1a49dd61c5824c4a8a637c1ee5d439efbcd70e56
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-18 20:56:11 +0300

    stat study chapter with description access modifiers in Java and added some classes

---
+++ src/main/java/AccessDemo.java
@@ -0,0 +1,27 @@
+
+class MyClass{
+    private int alpha;
+    public int beta;
+    int gamma;
+
+    void setAlpha(int a) {
+        alpha = a;
+    }
+
+    int getAlpha(){
+        return alpha;
+    }
+}
+
+public class AccessDemo {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+
+        obj.setAlpha(-99);
+        System.out.println("obj.alpha: " + obj.getAlpha());
+
+        obj.beta = 88;
+        obj.gamma = 99;
+    }
+
+}
---
+++ src/main/java/FSDemo.java
@@ -0,0 +1,59 @@
+class FailSoftArray{
+    private int a[];
+    private int errval;
+
+    public int length;
+
+    public FailSoftArray(int size, int errv) {
+        a = new int[size];
+        errval = errv;
+        length = size;
+    }
+
+    public int get(int index) {
+        if(indexOK(index)) return a[index];
+        return errval;
+    }
+
+    public boolean put(int index, int val) {
+        if(indexOK(index)) {
+            a[index] = val;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean indexOK(int index) {
+        if(index >=0 & index < length) return true;
+        return false;
+    }
+}
+
+
+public class FSDemo {
+    public static void main(String[] args) {
+        FailSoftArray fs = new FailSoftArray(5, -1);
+        int x;
+        System.out.println("Обработка ошибок без вывода отчёта.");
+        for(int i=0; i < (fs.length * 2); i++)
+            fs.put(i, i * 10);
+
+        for(int i=0; i < (fs.length * 2); i++) {
+            x = fs.get(i);
+            if(x != -1) System.out.println(x + " ");
+        }
+        System.out.println("");
+
+        System.out.println("\n Обработка ошибок с выводом отчета.");
+        for(int i=0; i <(fs.length * 2); i++)
+            if(!fs.put(i, i * 10))
+                System.out.println("Индекс " + i + " вне допустимого диапазона");
+
+        for(int i=0; i < (fs.length * 2); i++) {
+            x = fs.get(i);
+            if(x != -1) System.out.println(x + " ");
+            else
+                System.out.println("Индекс " + i + " вне допустимого диапазона");
+        }
+    }
+}
