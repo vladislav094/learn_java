51a7e82cf43a2facf09411c4f6ecc0246d12d8d3
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-19 14:38:05 +0300

    added solution for exercise with Latin Square

---
+++ src/main/java/LatinSquares.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Helper{
+    public static final int[][] makeLatinSquare(int n) {
+        int[][] latinSquare = new int[n][n];
+        for(int i = 0;  i < latinSquare.length; i++){
+            for(int j = 0; j < latinSquare[i].length; j++) {
+                latinSquare[i][j] = ((i + 1) + j) % n;
+                if (latinSquare[i][j] == 0) latinSquare[i][j] = n;
+            }
+        }
+        return latinSquare;
+    }
+}
+
+
+public class LatinSquares {
+    public static void main(String[] args) {
+        int[][] result;
+        result = Helper.makeLatinSquare(4);
+        for(int[] x: result){
+            for(int z: x){
+                System.out.print(z + " ");
+            }
+            System.out.println();
+        }
+    }
+}
