3d2e946a8dc26b1e4a9d442fec6b78000f77a55d
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-22 21:28:28 +0300

    added another one solution for the next exercise in codewars

--- src/main/java/OutOfSpace.java
+++ src/main/java/OutOfSpace.java
@@ -13,7 +13,45 @@ public class OutOfSpace {
         return expected;
     }
 
+    public static String toJadenCase(String phrase) {
+        // TODO put your code below this comment
+        if (phrase == null || phrase.isEmpty()) return null;
+//        if (phrase == null) return null;
+        else {
+        String [] chars = phrase.split(" ");
+        String[] result = new String[chars.length];
+        for (int i = 0; i < chars.length; i ++) {
+            String str = chars[i].substring(0, 1).toUpperCase() + chars[i].substring(1);
+            result[i] = str;
+        }
+        String r = String.join(" ", result);
+        System.out.println(r);
+        return null;
+        }
+    }
+
+    public static String disemvowel(String str) {
+        String vowels = "aqeuioAQEUIO";
+        String result = "";
+        for( int i = 0; i < str.length(); i++) {
+            char t = str.charAt(i);
+            if(vowels.indexOf(t) != -1) {
+                result += "";
+            }
+            else {
+                result += t;
+            }
+        }
+        System.out.println(result);
+        return result;
+    }
+
     public static void main(String[] args) {
-        OutOfSpace.spacey(array);
+        OutOfSpace.toJadenCase("Must return null when the arg is null");
+        OutOfSpace.toJadenCase("");
+        OutOfSpace.toJadenCase(null);
+//        disemvowel("This website is for losers LOL!");
+        disemvowel("No offense but,\nYour writing is among the worst I've ever read");
+        disemvowel("What are you, a communist?");
     }
 }
