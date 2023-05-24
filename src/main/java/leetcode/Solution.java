d12522ff2c4836802542a8b343bf13f659dabdfd
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-24 22:16:06 +0300

    added solution for exercise "longest-common-prefix" in leetcode

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -12,8 +12,24 @@ public class Solution {
         return true;
     }
 
+    public static String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for(int i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) !=0 ) {
+                prefix = prefix.substring(0, prefix.length() -1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+
     public static void main(String[] args) {
         System.out.println(isPalindrome(1212));
+
+        String[] s = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(s));
     }
 }
 
