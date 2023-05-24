a91469c535abe3494fe8d201bdf9ab38fac7ee35
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-24 23:25:53 +0300

    start solv next exercise with algorithm brackets

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -14,7 +14,7 @@ public class Solution {
 
     public static String longestCommonPrefix(String[] strs) {
         String prefix = strs[0];
-        for(int i = 0; i < strs.length; i++) {
+        for(int i = 1; i < strs.length; i++) {
             while (strs[i].indexOf(prefix) !=0 ) {
                 prefix = prefix.substring(0, prefix.length() -1);
                 if (prefix.isEmpty()) {
@@ -25,11 +25,26 @@ public class Solution {
         return prefix;
     }
 
+    public static boolean isValid(String s) {
+        String temp = s;
+        for(int i = 0, j = 1; i < temp.length() - 1; i += 2, j+= 2) {
+            System.out.println(temp.charAt(i) + " " + temp.charAt(j));
+            int t1 = temp.charAt(j);
+            int t2 = temp.charAt(i);
+            System.out.println(t1 + " " + t2);
+            if(t1 - t2 > 2) return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println(isPalindrome(1212));
 
         String[] s = {"flower","flow","flight"};
         System.out.println(longestCommonPrefix(s));
+
+//        System.out.println(isValid("(){}[]"));
+        System.out.println(isValid("(]"));
     }
 }
 
