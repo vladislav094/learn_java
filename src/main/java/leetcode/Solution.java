c12a9f7b7f0095f383fca9d0c80c0e0244c1dd43
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-24 21:50:54 +0300

    refactor function after view solution for this exercise in leetcode

--- src/main/java/leetcode/Solution.java
+++ src/main/java/leetcode/Solution.java
@@ -5,22 +5,15 @@ import java.util.Arrays;
 public class Solution {
     public static boolean isPalindrome(int x) {
         String str = String.valueOf(x);
-        String s = "";
-        String[] result1 = new String[str.length()];
-        String[] result2 = new String[str.length()];
-        char[] strArr_1 = new char[str.length()];
-        for (int i = 0; i < strArr_1.length; i++) {
-            s += str.charAt(i) + " ";
+        int l = str.length();
+        for(int i = 0; i < l/2; i++) {
+            if(str.charAt(i) != str.charAt(l - i - 1)) return false;
         }
-        result1 = s.split(" ");
-        for (int i = result1.length -1, j = 0; i >= 0; i--, j++){
-            result2[j] = result1[i];
-        }
-        return Arrays.equals(result1, result2);
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(1212));
     }
 }
 
