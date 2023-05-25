7fc9cdc49b30f0d51b1663255c74882d0cafd905
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-25 20:10:48 +0300

    added solution for task #28 remove-element in leetcode

--- src/main/java/leetcode/RemoveElement.java
+++ src/main/java/leetcode/RemoveElement.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class RemoveElement {
     public static int removeElement(int[] nums, int val) {
         int counter = 0;
-        for(int i = 1; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) {
                if(nums[i] != val) {
                    int t = nums[counter];
                    nums[counter] = nums[i];
@@ -15,14 +15,14 @@ public class RemoveElement {
                }
         }
         System.out.println(Arrays.toString(nums));
-        System.out.println(nums.length - counter);
-        return nums.length - counter;
+        System.out.println(counter);
+        return counter;
     }
 
     public static void main(String[] args) {
         int[] arr = {0,1,2,2,3,0,4,2};
         int[] arr2 = {3,2,2,3};
         removeElement(arr, 2);
-//        removeElement(arr2, 3);
+        removeElement(arr2, 3);
     }
 }
