88e1820e3994469c7f65404f5dbfe89ab2db0a22
Vladislav.H <vkhrabrov94@gmail.com>
2023-05-31 22:45:24 +0300

    added description for task #136

--- src/main/java/leetcode/SingleNumber.java
+++ src/main/java/leetcode/SingleNumber.java
@@ -3,11 +3,19 @@ package leetcode;
 import java.util.Arrays;
 
 public class SingleNumber {
+    /*
+    Используя побитовую операцию XOR сохраняем во временную переменную полученый результат для элементов массива
+    array = [4,1,2,1,2]
+    let sort array [1,1,2,2,4]
+    1 ^ 1 = 0
+    0 ^ 2 ^ 2 = 0 (0 результат 1 ^ 1 = 0)
+    0 ^ 4 = 4
+    (поэтому результат равен 4)
+     */
     public static int singleNumber(int[] nums) {
         Arrays.sort(nums);
         System.out.println(Arrays.toString(nums));
         int counter = 0;
-        int temp = 0;
         int startInt = nums[0];
         while (counter < nums.length -1) {
             System.out.println(startInt + " " + nums[counter+1]);
