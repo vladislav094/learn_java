99d32db65b4ae51c0a01c6319d339ff374309d7b
Vladislav.H <vkhrabrov94@gmail.com>
2023-06-01 14:51:02 +0300

    added several objects with separate thread

--- src/main/java/Threads/NewThreadImplementsRunnable.java
+++ src/main/java/Threads/NewThreadImplementsRunnable.java
@@ -1,36 +1,40 @@
 package Threads;
 
 public class NewThreadImplementsRunnable implements Runnable{
+    String name;    // имя потока
     Thread t;
 
-    NewThreadImplementsRunnable() {
+    NewThreadImplementsRunnable(String threadname) {
         // Создать новый, второй поток.
-        t = new Thread(this, "Demo Thread");
-        System.out.println("Дочерний поток: " + t);
+        name = threadname;
+        t = new Thread(this, name);
+        System.out.println("Новый поток: " + t);
     }
 
     public void run() {
         try {
             for(int i = 5; i > 0; i--) {
-                System.out.println("Sub thread: " + t);
+                System.out.println(name + ": " + t);
                 Thread.sleep(500);
             }
         } catch (InterruptedException e) {
-            System.out.println("Thread broken");
+            System.out.println(name + " broken");
         }
-        System.out.println("Finish sub thread");
+        System.out.println(name + " thread");
     }
 }
 
 class ThreadDemo {
     public static void main(String[] args) {
-        NewThreadImplementsRunnable nt = new NewThreadImplementsRunnable();
-        nt.t.start();
+        NewThreadImplementsRunnable nt1 = new NewThreadImplementsRunnable("One");
+        NewThreadImplementsRunnable nt2 = new NewThreadImplementsRunnable("Two");
+        NewThreadImplementsRunnable nt3 = new NewThreadImplementsRunnable("Three");
+        nt1.t.start();
+        nt2.t.start();
+        nt3.t.start();
         try {
-            for(int i = 5; i > 0; i--) {
-                System.out.println("Main Thread: " + i);
-                Thread.sleep(1000);
-            }
+            // Ожидать окончания остальных потоков.
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             System.out.println("Main Thread broken");
         }
