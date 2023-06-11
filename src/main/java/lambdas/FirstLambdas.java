15b19a6263fccbdeae63b1c04a1d425b7146d8f8
Vladislav.H <vkhrabrov94@gmail.com>
2023-06-11 13:25:18 +0300

    lamabdas and exceptions

--- src/main/java/lambdas/FirstLambdas.java
+++ src/main/java/lambdas/FirstLambdas.java
@@ -1,7 +1,9 @@
 package lambdas;
 import java.lang.Math;
+import java.util.Arrays;
+
 public class FirstLambdas {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws EmptyArrayException {
         MyNumber myNum;
         myNum = () -> 123.45;
 
@@ -19,14 +21,14 @@ public class FirstLambdas {
         if (isFactor.test(10, 2)) System.out.println("2 является множителем 10");
         if (!isFactor.test(10, 3)) System.out.println("3 не является множителем 10");
 
-        NumericFunc factorial = n -> {
+        GenericFunc<Integer> factorial = n -> {
             int result = 1;
             for (int i = 1; i <= n; i++)
                 result = i * result;
             return result;
         };
 
-        StringFunc revers = n -> {
+        GenericFunc<String> revers = n -> {
             String result = "";
             int i;
             for (i = n.length() -1; i >=0; i--)
@@ -36,12 +38,49 @@ public class FirstLambdas {
 
         System.out.println("Factorial number 3 equals: " + factorial.func(5));
         System.out.println("Revers word - lambdas is: " + revers.func("lambdas"));
+
+        String inStr = "Лямбда-выражение повышают эффективность Java";
+        String outStr;
+        System.out.println("Это исходная строка: " + inStr);
+        outStr = LambdasAsArgumentsDemo.stringOp((str) -> str.toUpperCase(), inStr);
+        System.out.println("Это строка прописными буквами: " + outStr);
+
+        outStr = LambdasAsArgumentsDemo.stringOp(n -> {
+            String result = "";
+            int i;
+            for (i = 0; i < n.length(); i++)
+                if (n.charAt(i) != ' ') result += n.charAt(i);
+            return result;
+        }, inStr);
+        System.out.println("Это строка без пробелов: " + outStr);
+
+
+        double[] values = {1.0, 2.0, 3.0, 4.0};
+
+        DoubleNumericArrayFunc average = n -> {
+            double sum = 0;
+            if (n.length == 0)
+                throw new EmptyArrayException();
+
+            for (int i = 0; i < n.length; i++)
+                sum += n[i];
+            return sum / n.length;
+        };
+
+        System.out.println("Среденее значение жлементов массива Double[] = " + average.func(values));
     }
+
+}
+
+class LambdasAsArgumentsDemo {
+    static String stringOp(StringFunc sf, String s){
+        return sf.func(s);
+    }
+
 }
 
 interface MyNumber {
     double getValue();
-
 }
 
 interface NumericTest {
@@ -52,10 +91,23 @@ interface NumericTest2 {
     boolean test(int n, int d);
 }
 
-interface NumericFunc{
-    int func(int n);
+/*Обощенный функциональный интерфейс. Позволяет избежать дублирование
+ кода при использовании лямбда-выражений с параметрами различного типа.
+ */
+interface GenericFunc<T>{
+    T func(T n);
 }
 
 interface StringFunc {
     String func(String n);
+}
+
+interface DoubleNumericArrayFunc {
+    double func(double[] n) throws EmptyArrayException;
+}
+
+class EmptyArrayException extends Exception {
+    EmptyArrayException() {
+        super("Массив пуст");
+    }
 }
