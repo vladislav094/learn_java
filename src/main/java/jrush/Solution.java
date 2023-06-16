5dd2a335699fc5c90a42e985de8c775794d726e2
Vladislav.H <vkhrabrov94@gmail.com>
2023-06-16 13:53:04 +0300

    start study on JavaRush

---
+++ src/main/java/jrush/Solution.java
@@ -0,0 +1,12 @@
+package jrush;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 4;
+        int z = 0;
+        String digits = String.valueOf(x + y) + "" + String.valueOf(z); //напишите тут ваш код
+        System.out.println(digits);
+    }
+}
