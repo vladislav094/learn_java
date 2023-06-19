1cb9fd0dd63d2987596e23682c1841f10358da9a
Vladislav.H <vkhrabrov94@gmail.com>
2023-06-19 22:28:03 +0300

    added new solution for exercise

--- src/main/java/jrush/Solution3.java
+++ src/main/java/jrush/Solution3.java
@@ -2,6 +2,21 @@ package jrush;
 
 public class Solution3 {
     public static void main(String[] args) {
-
+        int height = 1;
+        while (height <= 10) {
+            int width = 1;
+            while (width <= 20) {
+                if (height == 1 || height == 10) {
+                    System.out.print('Б');
+                } else if (width == 1 || width == 20) {
+                    System.out.print('Б');
+                } else {
+                    System.out.print(' ');
+                }
+                width++;
+            }
+            System.out.println();
+            height++;
+        }
     }
 }
