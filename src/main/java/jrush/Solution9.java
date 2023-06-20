3efd5d5647fb88bbf7b7099368ae9aa1ee24a79a
Vladislav.H <vkhrabrov94@gmail.com>
2023-06-20 21:22:59 +0300

    added another one solution for exercise

--- src/main/java/jrush/Solution9.java
+++ src/main/java/jrush/Solution9.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Solution9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int first = scanner.nextInt();
-        int second = scanner.nextInt();
-        double result = first * 1.0 / second;
+        boolean first = scanner.nextBoolean();
+        double glass = 0.5;
+        int result = 0;
+        if (first) {
+            result = (int) Math.ceil(glass);
+        } else result = (int) Math.floor(glass);
         System.out.println(result);
-
     }
 }
