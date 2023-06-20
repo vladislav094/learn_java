26d6af87e18f6434c6619bf230cd539117768dd6
Vladislav.H <vkhrabrov94@gmail.com>
2023-06-20 22:59:40 +0300

    added solution for exercise with multiplication table

--- src/main/java/jrush/Solution14.java
+++ src/main/java/jrush/Solution14.java
@@ -1,4 +1,27 @@
 package jrush;
 
+import java.util.Arrays;
+
 public class Solution14 {
+    public static int[][] MULTIPLICATION_TABLE;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        MULTIPLICATION_TABLE = new int[10][10];
+
+        int d = 0;
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 11; j ++) {
+                MULTIPLICATION_TABLE[d][j-1] = j * i;
+            }
+            d += 1;
+        }
+
+        for (int[] elt: MULTIPLICATION_TABLE){
+            for (int i: elt) {
+                System.out.print(i+ " ");
+            }
+            System.out.println();
+        }
+    }
 }
