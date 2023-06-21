83ba85d9ad1a363cdf8510c8dfe9f73d4e0add17
Vladislav.H <vkhrabrov94@gmail.com>
2023-06-21 16:41:49 +0300

    added function with detected exponenta and mantissa

---
+++ src/main/java/jrush/Solution22.java
@@ -0,0 +1,14 @@
+package jrush;
+
+public class Solution22 {
+    public static void main(String[] args) {
+        double[] number = {12742.0, 299792458.0, 238.0289, 0.085, 10000.0, 109.1678};
+
+        for (int i = 0; i < number.length; i++) {
+            double exp = Math.floor(Math.log10(number[i]));
+            double mats = number[i] / Math.pow(10, exp);
+            System.out.println(mats + " " + exp);
+
+        }
+    }
+}
