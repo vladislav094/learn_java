2de2654d1ea900ab74ed0cae0b653340221d9f79
Vladislav.H <vkhrabrov94@gmail.com>
2023-06-21 21:12:05 +0300

    continue study in javarush

---
+++ src/main/java/jrush/Solution24.java
@@ -0,0 +1,20 @@
+package jrush;
+
+public class Solution24 {
+    public static byte a;
+    public static short b;
+    public static int c;
+    public static long d;
+
+    public static void main(String[] args) {
+        setValues(100);
+    }
+
+    public static void setValues(long value){
+        //напишите тут ваш код
+        a = (byte) value;
+        b = (short) value;
+        c = (int) value;
+        d = value;
+    }
+}
