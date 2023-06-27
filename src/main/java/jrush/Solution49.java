44e6ddac1f82a7ce4fe164348fca34a9b0cefc5b
Vladislav.H <vkhrabrov94@gmail.com>
2023-06-27 22:33:15 +0300

    added last exercise today

---
+++ src/main/java/jrush/Solution49.java
@@ -0,0 +1,44 @@
+package jrush;
+
+import java.util.ArrayList;
+
+public class Solution49 {
+    public static void main(String[] args) {
+        var elements = new ArrayList<>();
+        elements.add("Привет");
+        elements.add(10);
+        elements.add(new Integer[15]);
+        elements.add(new LinkageError());
+        checkElementsType(elements);
+    }
+
+    public static void checkElementsType(ArrayList<Object> elements) {
+        //напишите тут ваш код
+        for (int i = 0; i < elements.size(); i++) {
+            if (elements.get(i) instanceof String) printString();
+            else if (elements.get(i) instanceof Integer) {
+                printInteger();
+            } else if (elements.get(i) instanceof Integer[]) {
+                printIntegerArray();
+            } else {
+                printUnknown();
+            }
+        }
+    }
+
+    public static void printString() {
+        System.out.println("Строка");
+    }
+
+    public static void printInteger() {
+        System.out.println("Целое число");
+    }
+
+    public static void printIntegerArray() {
+        System.out.println("Массив целых чисел");
+    }
+
+    public static void printUnknown() {
+        System.out.println("Другой тип данных");
+    }
+}
