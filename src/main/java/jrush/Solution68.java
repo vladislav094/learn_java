74d636495a5bde2d7cc234079d002c3b2e205160
Vladislav.H <vkhrabrov94@gmail.com>
2023-07-04 22:44:35 +0300

    start study System.input/System.outpu

---
+++ src/main/java/jrush/Solution68.java
@@ -0,0 +1,23 @@
+package jrush;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Solution68 {
+    public static void main(String[] args) throws IOException {
+        try (Scanner scanner = new Scanner(System.in);
+            BufferedReader bufferedReader = Files.newBufferedReader(Path.of(scanner.nextLine()))
+        )
+        {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
