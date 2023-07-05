8170815da77390c486a3d9b0e2db8f860dd4caa6
Vladislav.H <vkhrabrov94@gmail.com>
2023-07-05 23:02:54 +0300

    added last exercise today

--- src/main/java/jrush/Solution76.java
+++ src/main/java/jrush/Solution76.java
@@ -9,12 +9,24 @@ import java.util.Scanner;
 
 public class Solution76 {
     public static void main(String[] args) throws IOException {
-        byte[] bytes = args[0].getBytes();
-        try (InputStream stream = System.in;
-             Scanner scanner = new Scanner(stream)) {
-             Files.write(Path.of(scanner.nextLine()), bytes);
-        } catch (IOException e) {
-            System.out.println("Something went wrong : " + e);
-        }
+//        byte[] bytes = args[0].getBytes();
+//        try (InputStream stream = System.in;
+//             Scanner scanner = new Scanner(stream)) {
+//             Files.write(Path.of(scanner.nextLine()), bytes);
+//        } catch (IOException e) {
+//            System.out.println("Something went wrong : " + e);
+//        }
+
+
+//        char[] chars = args[0].toCharArray();
+//        try (InputStream stream = System.in;
+//             Scanner scanner = new Scanner(stream);
+//             BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(scanner.nextLine()))) {
+//            bufferedWriter.write(chars);
+//        } catch (IOException e) {
+//            System.out.println("Something went wrong : " + e);
+//        }
+
+
     }
 }
---
+++ src/main/java/jrush/Solution77.java
@@ -0,0 +1,26 @@
+package jrush;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Solution77 {
+    public static ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    public static PrintStream stream = new PrintStream(outputStream);
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        printSomething(scanner.nextLine());
+        String result = outputStream.toString();
+        outputStream.reset();
+        StringBuilder stringBuilder = new StringBuilder(result);
+        String reverse = stringBuilder.reverse().toString();
+        printSomething(reverse);
+        System.out.println(outputStream);
+    }
+
+    public static void printSomething(String str) {
+        stream.print(str);
+    }
+}
