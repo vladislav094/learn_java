83a826cb6b0d7509f5fc7c460da7607a3795a28a
Vladislav.H <vkhrabrov94@gmail.com>
2023-07-07 14:17:38 +0300

    added another task

---
+++ src/main/java/jrush/Solution89.java
@@ -0,0 +1,44 @@
+package jrush;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+public class Solution89 {
+
+    private static final int FRIDAY = 5;
+    private static final int SATURDAY = 6;
+    private static final int SUNDAY = 7;
+    private static final int WEEKEND_START_FRIDAY_CUT_OFF_HOUR = 22;
+    private static final int WEEKEND_END_SUNDAY_CUT_OFF_HOUR = 23;
+
+    public static void main(String[] args) {
+        List<LocalDateTime> dateTimeList = new ArrayList<>();
+        dateTimeList.add(LocalDateTime.of(2016, 4, 22, 18, 39));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 22, 21, 59));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 22, 22, 0));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 23, 5, 0));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 24, 8, 0));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 24, 22, 59));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 24, 23, 0));
+        dateTimeList.add(LocalDateTime.of(2016, 4, 25, 11, 5));
+
+        for (LocalDateTime dateTime : dateTimeList) {
+            System.out.println(dateTime + ", is weekend - " + isWeekend(dateTime));
+        }
+
+    }
+
+    public static boolean isWeekend(LocalDateTime dateTime) {
+        //напишите тут ваш код
+        int dayValue = dateTime.getDayOfWeek().getValue();
+        int countOfMinutes = dateTime.getHour() * 60 + dateTime.getMinute();
+        if (dayValue == SATURDAY) return true;
+        else if (dayValue == FRIDAY && countOfMinutes >= 1320) return true;
+        else if (dayValue == SUNDAY && countOfMinutes <= 1379 ) return true;
+        return false;
+    }
+}
+
