2d75321d2667c91683c7ec5fb48c580bf0ee436e
Vladislav.H <vkhrabrov94@gmail.com>
2023-07-11 21:56:09 +0300

    continue study DataTime in Java

--- src/main/java/jrush/Solution94.java
+++ src/main/java/jrush/Solution94.java
@@ -1,15 +1,24 @@
 package jrush;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.ZonedDateTime;
 
 public class Solution94 {
     static LocalDateTime localDateTime = LocalDateTime.now();
+    static ZonedDateTime zonedDateTime = ZonedDateTime.now();
 
     public static void main(String[] args) {
         //напишите тут ваш код
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy'г.' H'ч.'m'мин'");
         String text = dtf.format(localDateTime);
-        System.out.printf(text);
+        System.out.println(text);
+
+        DateTimeFormatter zdt = DateTimeFormatter.ofPattern("e d.M.yy HH:mm:ss.n\nVV");
+        String s = zdt.format(zonedDateTime);
+        System.out.println(s);
+
+
     }
 }
