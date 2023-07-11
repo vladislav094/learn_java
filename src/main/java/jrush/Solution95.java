b642f44a61611cbeba7b34a5c8aa8461eb9f3910
Vladislav.H <vkhrabrov94@gmail.com>
2023-07-11 22:11:53 +0300

    finish solv 17 lvl in javarush

---
+++ src/main/java/jrush/Solution95.java
@@ -0,0 +1,20 @@
+package jrush;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Solution95 {
+    static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
+    static String timeString = "13:30:45 23/02/2019";
+
+    public static void main(String[] args) {
+        LocalDateTime dateTime = LocalDateTime.parse(timeString, dateTimeFormatter);
+
+        System.out.println(dateTime);
+        LocalTime localDate = LocalTime.now();
+        System.out.println(localDate);
+    }
+}
