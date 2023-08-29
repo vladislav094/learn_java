849e14966e0e6dc0c3a283ad38f09ada04d1adbe
Vladislav.H <vkhrabrov94@gmail.com>
2023-08-29 23:35:51 +0300

    edit error in code

--- src/main/java/jrush/PatterAndMatcher.java
+++ src/main/java/jrush/PatterAndMatcher.java
@@ -1,15 +1,17 @@
 package jrush;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-public class PatterAndMatcher {
-        public static void main(String[] args) {
-            String str = "Hello 123 world 456";
-            Pattern pattern = Pattern.compile("\\d+"); // "\\d+" соответствует одной или более цифрам
-            Matcher matcher = pattern.matcher(str);
 
-            while (matcher.find()) {
-                System.out.println(matcher.group()); // Выводит найденное число
-            }
+public class PatterAndMatcher {
+    public static void main(String[] args) {
+        String str = "Hello 123 world 456";
+        Pattern pattern = Pattern.compile("\\d+"); // "\\d+" соответствует одной или более цифрам
+        Matcher matcher = pattern.matcher(str);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group()); // Выводит найденное число
         }
     }
 }
+
